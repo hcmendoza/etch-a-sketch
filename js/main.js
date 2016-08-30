a9fcdc90c7cdc7b41443b84bd4d4d2e5605f66da
Hugo Mendoza <hugo.mendoza.ce@gmail.com>
2016-08-29 17:28:45 -0700

    update style

--- js/main.js
+++ js/main.js
@@ -1,19 +1,18 @@
 var main = function() {
-
-	// Button Click - Start/Reset
-	$('#gameStart').on('click', function() {
-
-		//Clear the board
+//Grid Constructor
+	var constructor = function() {
+	//Clear the board
 		$('.container').empty();
 
+	//USER input
 		var sides = prompt("How many squares per side? Please enter a number between 1 and 100.");
 		var size = 960/sides;
 
+	//Build the grid
 		var rows = function() {
 			for(var x = 0; x < sides; x++) {
 				$('.container').append('<div class="row"></div>');
 			}
-			return false;
 		};
 
 		var columns = function() {
@@ -21,20 +20,64 @@ var main = function() {
 				$('.row').append('<div class="tile"></div>');
 			}
 			$('.tile').css({'width': size,'height': size});
-			return false;
 		};
 
-		//Create the grid
 		rows();
 		columns();
+	};
 
-		//Track mouse trail
+//CLEAR
+	$('#clear').on('click', function() {
+		//Clear the board
+		$('.container').empty();
+	});
+
+//GRAY
+	$('#gray').on('click', function() {
+		constructor();
 		$('.tile').on('mouseleave', function() {
 			$(this).addClass('selected');
 		});
 	});
-};
 
+//RANDOM COLOR
+	$('#random').on('click', function() {
+		constructor();
+		var r = Math.floor(Math.random()*256);
+		var g = Math.floor(Math.random()*256);
+		var b = Math.floor(Math.random()*256);
+		var randomColor = "rgb("+r+ ", " +g+", "+b+")";
+		$('.tile').on('mouseenter', function() {
+			$(this).css('background-color', randomColor);
+		});
+	});
+
+//COLORFUL
+	$('#colorful').on('click', function() {
+		constructor();
+		$('.tile').on('mouseenter', function() {
+			var r = Math.floor(Math.random()*256);
+			var g = Math.floor(Math.random()*256);
+			var b = Math.floor(Math.random()*256);
+			var randomColor = "rgb("+r+ ", " +g+", "+b+")";
+			$(this).css('background-color', randomColor);
+		});
+	});
+
+//OPACITY
+	$('#opacity').on('click', function() {
+		
+		constructor();
+		$('.tile').css({'background-color': '#000', 'opacity': '0'});
+
+		$('.tile').on('mouseenter', function() {
+			var opacity = +$(this).css('opacity');
+			opacity += 0.1;
+			$(this).css('opacity', opacity);
+		});
+
+	});
+};
 
 
 $(document).ready(main);
